77a232abf91756e5e4bda47b094c38b99239bb46
kenjinote <kenji256@gmail.com>
2017-07-11 10:16:15 +0900

    initial commit

---
+++ Source.cpp
@@ -0,0 +1,105 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(lib, "psapi")
+
+#include <windows.h>
+#include <psapi.h>
+
+TCHAR szClassName[] = TEXT("Window");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hButton;
+	static HWND hList;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hButton = CreateWindow(TEXT("BUTTON"), TEXT("メモリの縮小化"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hList = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("LISTBOX"), 0, WS_VISIBLE | WS_CHILD | WS_VSCROLL | LBS_NOINTEGRALHEIGHT, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		break;
+	case WM_SIZE:
+		MoveWindow(hButton, 10, 10, 256, 32, TRUE);
+		MoveWindow(hList, 0, 50, LOWORD(lParam), HIWORD(lParam) - 50, TRUE);
+		break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK)
+		{
+			SendMessage(hList, LB_RESETCONTENT, 0, 0);
+			DWORD aProcesses[1024], cbNeeded, cProcesses;
+			if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded))
+			{
+				return 0;
+			}
+			cProcesses = cbNeeded / sizeof(DWORD);
+			for (unsigned int i = 0; i < cProcesses; i++)
+			{
+				if (aProcesses[i] != 0)
+				{
+					TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
+					HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, aProcesses[i]);
+					BOOL bResult = FALSE;
+					if (NULL != hProcess)
+					{
+						HMODULE hMod;
+						DWORD cbNeeded;
+						if (EnumProcessModules(hProcess, &hMod, sizeof(hMod), &cbNeeded))
+						{
+							GetModuleBaseName(hProcess, hMod, szProcessName, sizeof(szProcessName) / sizeof(TCHAR));
+						}
+						bResult = EmptyWorkingSet(hProcess);
+					}
+					TCHAR szText[1024];
+					wsprintf(szText, TEXT("%s (PID: %u) のメモリを最小化%s"), szProcessName, aProcesses[i], bResult ? TEXT("しました。") : TEXT("できませんでした。"));
+					SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)szText);
+					CloseHandle(hProcess);
+				}
+			}
+			MessageBox(hWnd, TEXT("完了"), TEXT("確認"), 0);
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("メモリの最小化"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
